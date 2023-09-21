bd81156219b959f3cfd20af1ff35404f94dfa682
vi1996 <victorpalechor@unicomfacauca.edu.co>
2023-09-20 22:26:14 -0500

    crear repository y las clases de servicio, el metodo show de productoController

--- src/main/java/com/curso/ecommerce/controller/ProductoController.java
+++ src/main/java/com/curso/ecommerce/controller/ProductoController.java
@@ -5,12 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/administrador")
-public class AdministradorController {
+@RequestMapping("/productos")
+public class ProductoController {
 
 	@GetMapping("")
-	public String home() {
-		return "administrador/home";
+	public String show() {
+		return "productos/show";
 	}
 
 }
